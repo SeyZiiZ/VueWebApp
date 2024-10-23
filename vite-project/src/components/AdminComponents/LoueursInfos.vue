2d091cdea4c1f2de69730859e5c57c3886aebc67
Seyy <147831465+SeyZiiZ@users.noreply.github.com>
2024-10-23 22:43:29 +0200

    Delete vite-project directory

--- vite-project/src/components/AdminComponents/LoueursInfos.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <h1>Informations Sur Les Loueurs : </h1>
-    <h2>Cliquez sur <a href="http://localhost:3000/api/users" target="_blank"> ce lien </a> pour voir la liste des personnes inscrites sur le site</h2>
-</template>
-
-<script setup>
-
-</script>
